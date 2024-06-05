659b1f11da5ed9ca571c9db5ac84a084a53324f1
Manuja Dewmina <manuja1bc@gmail.com>
2024-06-05 16:04:00 +0530

    Connect websocket servcer with api gateway

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import asyncio
 import websockets
-import datetime
 import logging
+import requests
+import json
+import datetime
 
 app = Flask(__name__)
 CORS(app) 
@@ -12,24 +14,53 @@ logging.basicConfig(level=logging.INFO)
 
 clients = set()
 
+log_id = None
+box_id = None
+item_type = None
+user_id = None
+start_time = None
+logs = []
+
 async def handler(websocket, path):
+    global log_id, box_id, item_type, user_id, start_time, logs
     clients.add(websocket)
+    initial_data = await websocket.recv()
+    get_data(initial_data)
     try:
         while True:
             data = await websocket.recv()
-            log_received_data(data) 
-            reply = f"Data received as: {data}!"
-            await websocket.send(reply)
-    except websockets.ConnectionClosed:
+            log_info()
+    finally:
         clients.remove(websocket)
+        send_data_to_backend()
 
-def log_received_data(data):
-    log_id = None
-    box_id = None
-    item_type = None
-    user_id = None
-    start_time = None
-    
+def send_data_to_backend():
+    logging.info("Sending data to backend")
+    global log_id, box_id, item_type, user_id, start_time, logs
+    logs_str = " ".join(logs)
+
+    end_time = datetime.datetime.now().isoformat()
+
+    payload = {
+        "logId": log_id,
+        "boxId": box_id,
+        "itemType": item_type,
+        "userId": user_id,
+        "totalCount": 0,
+        "startTime": start_time,
+        "endTime": end_time,
+        "fullLogFile": logs_str 
+    }
+
+    try:
+        response = requests.post("http://localhost:8007/log/api/Log", json=payload)
+        response.raise_for_status()
+        logging.info(f"Successfully sent data to endpoint: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error sending data to endpoint: {e}")
+
+def get_data(data):
+    global log_id, box_id, item_type, user_id, start_time
     data_lines = data.decode("utf-8").split("\n")
     for line in data_lines:
         if line.startswith("LogId:"):
@@ -42,8 +73,15 @@ def log_received_data(data):
             user_id = line.split(":")[1]
         elif line.startswith("StartTime:"):
             start_time = line.split(":")[1]
-        logging.info(f"LogId: {log_id}, BoxId: {box_id}, ItemType: {item_type}, UserId: {user_id}, StartTime: {start_time}")
 
+def log_info():
+    global log_id, box_id, item_type, user_id
+    log_time = datetime.datetime.now().isoformat()
+    logging.info(f"LogId: {log_id}, BoxId: {box_id}, ItemType: {item_type}, UserId: {user_id}, LogTime: {log_time}")
+
+    log = f"LogId: {log_id}, BoxId: {box_id}, ItemType: {item_type}, UserId: {user_id}, LogTime: {log_time}"
+    logs.append(log + "\n")
+    
 if __name__ == "__main__":
     start_server = websockets.serve(handler, host='0.0.0.0', port=8009)
     asyncio.get_event_loop().run_until_complete(start_server)
